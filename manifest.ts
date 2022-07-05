08005d6c201dbfd05d26b372d90b611b529e44f0
echoeureka <iherewithmyheart@gmail.com>
2022-07-05 17:47:31 +0800

    :package: v0.0.2

--- manifest.ts
+++ manifest.ts
@@ -3,7 +3,7 @@ import { ManifestV3Export } from '@crxjs/vite-plugin'
 const manifest: ManifestV3Export = {
   manifest_version: 3,
   name: 'Hitokoto',
-  version: '0.0.1',
+  version: '0.0.2',
   chrome_url_overrides: {
     newtab: 'index.html'
   }
